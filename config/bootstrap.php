eeed02329241ad26b272de14ae043733085e349b
michaelhue <hello@michaelhue.com>
2012-12-05 12:03:21 +0100

    Ensure the class only binds itself to controllers.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,7 +4,13 @@ use li3_flash_message\extensions\storage\FlashMessage;
 use lithium\action\Dispatcher;
 
 Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
-	return FlashMessage::bindTo($chain->next($self, $params, $chain));
+	$object = $chain->next($self, $params, $chain);
+
+	if (is_a($object, 'lithium\action\Controller')) {
+		return FlashMessage::bindTo($object);
+	}
+
+	return $object;
 });
 
 ?>
